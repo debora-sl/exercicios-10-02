5a67861f96afa78c20666aa54242833e9f4474ca
Debora Souza <debora.souzalima22@gmail.com>
2025-02-14 19:51:59 -0300

    Conslusão do exercicio 6.

--- js/exercicio-6.js
+++ js/exercicio-6.js
@@ -1,4 +1,30 @@
-console.log('Exercicio 6');
+// pegando o botão
+let incluirCarro = document.getElementById("incluirCarro");
+
+// adicionando o evento de click
+incluirCarro.addEventListener('click', function () {
+    var carros = {
+        marca: '',
+        modelo: '',
+        ano: ''
+    }
+
+    carros.marca = document.getElementById("marca").value;
+    carros.modelo = document.getElementById("modelo").value;
+    carros.ano = document.getElementById("ano").value;
+
+    // inserindo o resultado no HTML
+    var resultadoHTML = document.getElementById("resultadoHTML").innerHTML = `Carro:{ Marca: ${carros.marca}, Modelo: ${carros.modelo} e Ano: ${carros.ano} }`
+
+    limparInputs();
+});
+
+// função para limpar os campos, após o usário inserir os dados
+function limparInputs() {
+    document.getElementById("marca").value = '';
+    document.getElementById("modelo").value = '';
+    document.getElementById("ano").value = ''
+}
 
 
 
